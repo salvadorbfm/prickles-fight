df52ae51f94ef76d2f8cf2a70b373f29b3847cf5
Salvador Elizarraras <salvadorbfm@gmail.com>
2012-11-27 08:42:56 -0600

    Just a few update

--- js/game_sem.js
+++ js/game_sem.js
@@ -11,32 +11,32 @@ var Game = (function() {
 
         self.width = config.width || 20;
         self.height = config.height || 20;
-        self.speed_x = config.speed_x || 1;
-        self.speed_y = config.speed_y || 1;
+        self.speed_x = config.speed_x || 1.0;
+        self.speed_y = config.speed_y || 1.0;
 
         self.boost  = config.boost || 0;
 
-        self.direction_x = 1;
-        self.direction_y = 1;
+        self.direction_x = 1.0;
+        self.direction_y = 1.0;
         //Math.floor(self.speed_y/Math.abs(self.speed_y));
 
         self.RGB = config.RGB || [200,200,200];
 
         self.drawable = true;
 
-        self.getCenterX = function(){
+        self.get_center_x = function(){
             return self.x + self.width/2;
         };
 
-        self.getCenterY = function(){
+        self.get_center_y = function(){
             return self.y + self.height/2;
         };
 
         self.calculate_direction = function(val){
             if(val>600 || val <0){
-                return -1;
+                return -1.0;
             }else{
-                return 1;
+                return 1.0;
             }
         };
 
@@ -59,19 +59,32 @@ var Game = (function() {
             }
         };
 
-        self.update_position = function(){
-            self.x = self.x + self.speed_x + self.boost;
-            self.y = self.y + self.speed_y + self.boost;
-            self.boost = 0;
-        };
 
         self.update_speed = function(){
             self.direction_x = self.calculate_direction(self.x);
             self.direction_y = self.calculate_direction(self.y);
+
+
             self.speed_x = self.speed_x * self.direction_x;
             self.speed_y = self.speed_y * self.direction_y;
         };
 
+        self.check_boundary = function(){
+
+        };
+        self.update_position = function(){
+
+            self.direction_x *= self.calculate_direction(self.x);
+            self.direction_y *= self.calculate_direction(self.y);
+
+            self.speed_x *= self.direction_x;
+            self.speed_y *= self.direction_y;
+
+            self.x += self.speed_x + self.boost;
+            self.y += self.speed_y + self.boost;
+            console.log("//"+self.x+":"+self.y);
+            self.boost = 0;
+        };
 
         return self;
     };
@@ -98,35 +111,44 @@ var Game = (function() {
         // search(happy,sprites)
         // This function looks for the closest sprite
         self.search = function(happy, sprites){
-            var center = {x:self.getCenterX(), y:self.getCenterY()};
+            var center = {x:self.get_center_x(), y:self.get_center_y()};
             var min = 0;
-            var distance = 0;
+            var distance = {};
+            var magnitude = 0;
+            var distance_to_enemy = {};
+            var normalized = {};
 
-            min = sprites[sprites.length - 1];
+            min = 99999.0;
             for (var i = sprites.length - 1; i >= 0; i--) {
                 if(sprites.drawable == false) continue;
 
                 distance = util.get_distance(
                             center,
                             {
-                            x:sprites[i].getCenterX(),
-                            y:sprites[i].getCenterY()
+                            x:sprites[i].x,
+                            y:sprites[i].y
                             }
                             );
 
-                if( distance<min ){
-                    min = distance;
+                magnitude = util.get_magnitude(distance);
+                if( magnitude<min ){
+                    min = magnitude;
                     self.sprite_target = i;
-                    self.direction_x = sprites[i].direction_x;
-                    self.direction_y = sprites[i].direction_y;
+                    //console.log("Evil  search()... mag:"+magnitude+" min:"+min);
+                    console.log(self.x+":"+self.y);
+                    distance_to_enemy = distance;
                 }
             }
+            normalized = util.normalize(distance_to_enemy);
+            self.direction_x = normalized.x;
+            self.direction_y = normalized.y;
+            self.update_position();
         };
 
         self.destroy = function(happy, sprites, phys){
-            sprites = sprites || new Array();
-            happy = happy || {};
-            phys = phys || {};
+            var sprites = sprites || new Array();
+            var happy = happy || {};
+            var phys = phys || {};
 
             if(self.sprite_target >= 0) {
                 if( phys.check_collision( self, sprites[self.sprite_target]) ){
@@ -135,16 +157,6 @@ var Game = (function() {
             }
         };
 
-        self.update_speed = function(){
-            if(self.calculate_direction(self.x) < 0)
-                self.direction_x = -1;
-
-            if(self.calculate_direction(self.y) < 0)
-                self.direction_y = -1;
-
-            self.speed_x = self.speed_x * self.direction_x;
-            self.speed_y = self.speed_y * self.direction_y;
-        };
         return self;
     };
 
@@ -161,12 +173,32 @@ var Game = (function() {
         };
 
         self.get_distance = function(p1,p2){
-            return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+            //return Math.sqrt((p2.x-p1.x)*(p2.x-p1.x) + (p2.y-p1.y)*(p2.y-p1.y));
+            var distance = {};
+            distance.x = p2.x - p1.x;
+            distance.y = p2.y - p1.y;
+            return distance;
         };
+
+        self.get_magnitude = function(p){
+            var magnitude =  Math.sqrt(p.x*p.x + p.y*p.y);
+
+            if(magnitude < 0.00001)
+                return 1;
+            else
+                return magnitude;
+        };
+
+        self.normalize = function(p){
+            magnitude = self.get_magnitude(p);
+            return {
+                x: p.x/magnitude,
+                y: p.y/magnitude
+            }
+        }
         return self;
     };
     var Phys = function(config){
-        // Colisiones entre N Objetos circulares
         config = config || {};
 
         var self = {};
@@ -174,8 +206,8 @@ var Game = (function() {
 
         // Checking collision between two sprites
         self.check_collision = function(spriteA, spriteB){
-            spriteA = spriteA || {};  // Here, I should check the error of having a "null" sprite
-            spriteB = spriteB || {};
+            var spriteA = spriteA || {};  // Here, I should check the error of having a "null" sprite
+            var spriteB = spriteB || {};
             var radA = 0;
             var radB = 0;
             var centerA = {};
@@ -184,8 +216,8 @@ var Game = (function() {
 
             radA = Math.floor(spriteA.width/2);
             radB = Math.floor(spriteB.width/2);
-            centerA = {x:spriteA.getCenterX(), y:spriteA.getCenterY()};
-            centerB = {x:spriteB.getCenterX(), y:spriteB.getCenterY()};
+            centerA = {x:spriteA.get_center_x(), y:spriteA.get_center_y()};
+            centerB = {x:spriteB.get_center_x(), y:spriteB.get_center_y()};
 
             distance = util.get_distance(centerA,centerB);
 
@@ -289,6 +321,7 @@ var Game = (function() {
             processing.fill(happy.RGB[0], happy.RGB[1], happy.RGB[2]);
             processing.ellipse(happy.x, happy.y, happy.width, happy.height);
             processing.fill(evil.RGB[0], evil.RGB[1], evil.RGB[2]);
+            //debugger;
             processing.ellipse(evil.x, evil.y, evil.width, evil.height);
 
             for(var i=0; i<10; i++){
@@ -299,20 +332,18 @@ var Game = (function() {
 
             phys.check_collisions(sprites);
 
-            happy.update_speed();
             evil.search(happy, sprites);
-            evil.destroy(happy, sprites, phys);
-            evil.update_speed();
+            //debugger;
+            //evil.update_position();
+            //evil.destroy(happy, sprites, phys);
 
             happy.update_position();
-            evil.update_position();
 
 
 
             for(var i=0; i<10; i++){
                 if(sprites[i].drawable == false) continue;
                 sprites[i].update_position();
-                sprites[i].update_speed();
             }
         };
     };
